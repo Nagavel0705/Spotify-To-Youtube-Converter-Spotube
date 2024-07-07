ceaaa763ea8b2651384aad44b2f423f75c661a75
Nagavel <nagavel2010376@ssn.edu.in>
2024-07-07 10:08:23 +0530

    explicitly set views directory to views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 require("dotenv").config();
 const SpotifyWebApi = require("spotify-web-api-node");
 const youtube = require("@googleapis/youtube").youtube("v3");
@@ -24,6 +25,8 @@ const User = mongoose.model("User", {
 
 const app = express();
 app.use(express.static("public"));
+
+app.set('views', path.join(__dirname, './views'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
